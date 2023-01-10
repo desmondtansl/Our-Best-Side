41bf45e92bfcf19c8cb42ae52e44ec742545c356
Desmond Tan <desmondtansl90@gmail.com>
2023-01-10 16:11:20 +0800

    carousel component half done

---
+++ client/components/Carousel.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import styled from "styled-components";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+function Carousel() {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+  };
+
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <KeyboardArrowLeftIcon />
+      </Arrow>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <KeyboardArrowRightIcon />
+      </Arrow>
+    </Container>
+  );
+}
+export default Carousel;
--- client/pages/Homepage.jsx
+++ client/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Annoucement from "../components/Announcement";
 import Navbar from "../components/Navbar";
+import Carousel from "../components/Carousel";
 
 function Homepage() {
   return (
     <div>
       <Annoucement />
       <Navbar />
+      <Carousel />
     </div>
   );
 }
