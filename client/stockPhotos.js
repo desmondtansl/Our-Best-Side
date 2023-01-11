0682d86c5930ac426d5730ccab2486e085e8fd86
Desmond <desmondtansl90@gmail.com>
2023-01-11 11:52:35 +0800

    added stock images for Carousel slider

---
+++ client/stockPhotos.js
@@ -0,0 +1,25 @@
+export const carouselPhotos = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/842811/pexels-photo-842811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "STAY IN TREND",
+    description: "Browse our latest menswear collection",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/247298/pexels-photo-247298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "GET READY FOR SPRING",
+    description:
+      "Celebrate the Lunar New Year in style with our latest CNY collection",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://chinesenewyear.imgix.net/assets/images/chinese-new-year--mobile.png?q=25&w=1920&auto=format%201920w",
+    title: "HAPPY CHINESE NEW YEAR",
+    description:
+      "Wishing all our shoppers an auspicious and prosperous New Year!",
+    bg: "fcf1ed",
+  },
+];
