8f45569e6a15c88057f06ec49e39b9fdcee07663
Desmond <desmondtansl90@gmail.com>
2023-01-12 16:18:49 +0800

    Newsletter component completed

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -1,8 +1,52 @@
 import styled from "styled-components";
+import SendIcon from "@mui/icons-material/Send";
 
-const Container = styled.div``;
+const Container = styled.div`
+  height: 60vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  margin-bottom: 20px;
+`;
+
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+`;
+
+const Input = styled.input`
+  border: none;
+  flex: 7;
+  padding-left: 20px;
+  font-style: italic;
+`;
+
+const Button = styled.button`
+  flex: 1;
+  border: none;
+  cursor: pointer;
+`;
+
 function Newsletter() {
-  return <Container>Newsletter</Container>;
+  return (
+    <Container>
+      <Title>Subscribe to get latest updates!</Title>
+      <InputContainer>
+        <Input placeholder="e.g. example@example.com" />
+        <Button>
+          <SendIcon />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
 }
 
 export default Newsletter;
