da4d08781dcb15cab1f683743fa523a222c06524
Desmond <desmondtansl90@gmail.com>
2023-01-14 16:19:25 +0800

    added Login links to Navbar component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,10 +3,25 @@ import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+`;
 
 function Login() {
-  return <Container>Login</Container>;
+  return (
+    <Container>
+      <Wrapper>Login</Wrapper>
+    </Container>
+  );
 }
 
 export default Login;
