e8c45256483ad3b1d93df09d8bd75c820140459f
Desmond <desmondtansl90@gmail.com>
2023-01-15 20:21:11 +0800

    added Login route and tested in Insomnia

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -66,4 +66,63 @@ router.post(
   }
 );
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({
+      data: "",
+      error: [
+        {
+          msg: "Invalid credentials",
+        },
+      ],
+    });
+  }
+  try {
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        data: "",
+        error: [
+          {
+            msg: "Invalid credentials",
+          },
+        ],
+      });
+    }
+
+    const token = await JWT.sign(
+      { email: user.email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 86400,
+      }
+    );
+
+    return res.status(200).json({
+      data: {
+        token,
+        user: {
+          id: user.id,
+          email: user.email,
+        },
+      },
+      error: "",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      data: "",
+      error: [
+        {
+          msg: error.message,
+        },
+      ],
+    });
+  }
+});
+
 export default router;
